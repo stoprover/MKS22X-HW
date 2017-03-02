90294c04c129782ba69c1e76fc6250c8157af90f
stoprover <steventoprover@gmail.com>
2017-03-02 14:49:56 -0500

    this is almost working but actually terrible

--- 04Maze/Maze.java
+++ 04Maze/Maze.java
@@ -5,9 +5,11 @@ public class Maze{
     private char[][] maze;
     private boolean animate;
 
-    public Maze(String filename) throws FileNotFoundException{
+    public Maze(String filename){
 	animate = false;
-	ReadFile R = new ReadFile(filename);
+	//	try{
+	    ReadFile R = new ReadFile(filename);
+	    //throw new FileNotFoundException();
         /**File infile = new File(filename);// can be a path"/full/path/to/file.txt" 
         Scanner inf = new Scanner (infile);
         int lineNumber = 1;
@@ -25,6 +27,13 @@ public class Maze{
 	    r++;
 	}
 	maze = new char[c][r];	
-	while (inf.hasNextChar())//now we start filling maze   */   
- 
-    }  
+	while (inf.hasNextChar())//now we start filling maze   */
+	//}catch (FileNotFoundException e){
+	//       System.out.println("File does not exist.");
+	//    	}   
+	//throw new FileNotFoundException();
+    }
+    public static void main(String[]args){
+	Maze M = new Maze("input.txt");
+    }
+}  
--- 04Maze/ReadFile.java
+++ 04Maze/ReadFile.java
@@ -3,31 +3,65 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class ReadFile{
     private String fileName;
+    private int numLines = 0;
+    private int numCols = 0;
     private char[][] maze;
     public ReadFile(String name){
     	fileName = name;
     }
     //public static void main(String args[]) throws FileNotFoundException {
     public void readIt() throws FileNotFoundException{
-
-        //instead of a try/catch, you can throw the FileNotFoundException.
-        File infile = new File(fileName);// can be a path"/full/path/to/file.txt" 
-        Scanner inf = new Scanner (infile);
-        int lineNumber = 1;
-	int numLines = 0;
-	int numCols = 0;
-        while(inf.hasNextLine()){
-	    numLines++;
-	    //if (numLines == 0){
-	    //for (int i = 0; i < 
-	    //do string line = whatever, then do for int i = 0, i < line.length; i++ --> numCols++;
-	    //then that's
-            String line = inf.nextLine();
-            System.out.println(line);
-        }       
+	try{
+	    File infile = new File(fileName);
+	    Scanner inf = new Scanner (infile);
+	    // int lineNumber = 1;
+	    while(inf.hasNextLine()){
+		numLines++;
+		//if (numLines == 0){
+		//for (int i = 0; i < 
+		//do string line = whatever, then do for int i = 0, i < line.length; i++ --> numCols++;
+		//then that's
+		String line = inf.nextLine();
+		numCols = line.length();
+		//System.out.println(line);
+		
+	    } 
+	    maze = new char[numLines][numCols];
+	    //while(inf.hasNextLine()){
+	    //String line = inf.nextLine();
+		for (int i = 0; i < numCols; i++){
+		    String l = inf.nextLine();
+		    for (int j = 0; j < numLines; j++){
+			maze[i][j] = l.charAt(j);
+		    }
+		}
+throw new FileNotFoundException();
+	}catch(FileNotFoundException e){
+	    System.out.println("file does not exist.");
+	}
+    }
+    public String toString(){
+	String sum = "";
+	for (int i = 0; i < maze.length; i++){
+	    for (int j = 0; j < maze[0].length; j++){
+		sum += maze[i][j];
+	    }
+	    sum += "\n";
+	}
+	return sum;
+    }
+    //public void setUpMaze(){
+    //	maze = new char[numLines][numCols];
+    //c
+    public int getLines(){
+	return numLines;
+    }
+    public int getCols(){
+	return numCols;
     }
     public static void main(String args[]) throws FileNotFoundException{
 	ReadFile R = new ReadFile("input.txt");	
 	R.readIt();
+	System.out.println(R.toString());
     }
 }

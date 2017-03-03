91f12765e6ce13160979c97284a00997c044b692
Galina Gorina <Galina Gorina>
2017-03-02 22:02:59 -0500

    fixed up any issues

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -4,12 +4,21 @@ public class Recursion{
 	return "Toprover,Steven";
     }
     public static boolean isCloseEnough(double a, double b){
-	if (a < b){
-	    return (Math.abs(a - b)) < a * .000000001;
+	if (a == 0.0 && b == 0.0){
+	    return true;
 	}
-	else{
-	    return (Math.abs(a - b)) < b * .000000001;
+	if (a == 0.0){
+	    return b < 0.00000000001;
 	}
+	if (b == 0.0){
+	    return a < 0.00000000001;
+	}
+	//if (a < b){
+	    return (Math.abs(a - b)) < a *  0.00000000001;
+	    //}
+	    //else{
+	    //return (Math.abs(a - b)) < b * .000000001;
+	    //}
     }
     public static double betterGuess(double num, double orig){
 	if (isCloseEnough(orig*orig, num)){
@@ -21,14 +30,17 @@ public class Recursion{
 	if (n < 0){
 	    throw new IllegalArgumentException("The input is negative.");
 	}//exception and whatnot
+	if (n == 0.0){
+	    return 0.0;
+	}
 	return betterGuess(n, 1);
     }
     public static void main (String[]args){
 	    //the following are test cases
-	/**System.out.println(name());
+	/**	System.out.println(name());
 	System.out.println(isCloseEnough(.10000000000, .10000000010));
 	System.out.println(sqrt(100));
-	System.out.println(sqrt(-4));
+	//System.out.println(sqrt(-4));
 	System.out.println(sqrt(4));*/
     }
 

6771f9ea22a4b404d0e22712dde5a0ecb472aea6
stoprover <steventoprover@gmail.com>
2017-03-06 14:49:56 -0500

    more work

--- 05Cows/Makelake.java
+++ 05Cows/Makelake.java
@@ -28,12 +28,17 @@ public class Makelake{
 	s = s.substring(s.indexOf(" ") + 1);
 	n = Integer.parseInt(s.substring(0,s.indexOf(" ")));
 	lakeMap = new int[r][c];
-	int currentRow = 1;
+	int currentRow = 0;
 	s = "";
-	while (currentRow < 1 + r){
+	while (currentRow < r){
 	    if (sc.hasNextLine()){
-		s += sc.nextLine();
+		s = sc.nextLine();
+		int i = 0;
+		while (s.length() > -1) {
+		    lakeMap[currentRow][i] = Integer.parseInt(s.substring(0, indexOf(" ")));
+		    i ++;
+		}
 	    }
 	    else throw new IllegalArgumentException("Lake grid not formatted correctly");
-	    for (
+	}
 }

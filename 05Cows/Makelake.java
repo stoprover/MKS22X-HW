0dcb7073fedf5f75cd695feabf65122652455561
stoprover <steventoprover@gmail.com>
2017-03-07 14:47:07 -0500

    more stuff

--- 05Cows/Makelake.java
+++ 05Cows/Makelake.java
@@ -1,12 +1,10 @@
 //R is row, C is column, E is final elevation, number of commands to stomp
-public class Makelake{
-    private int r;
-    private int c;
-    private int e;
-    private int n;
-    private int[][] lakeMap;
+public class USACO{
 
-    public Makelake(String fileName){
+    public static int bronze(String fileName){
+	int r, c, e, n;
+        int[][] lakeMap;
+	String[] instructions;
 	try{
 	    Scanner sc = new Scanner(new File(fileName));
 	    if (sc.hasNextLine()){
@@ -26,19 +24,53 @@ public class Makelake{
 	s = s.substring(s.indexOf(" ") + 1);
 	e = Integer.parseInt(s.substring(0,s.indexOf(" ")));
 	s = s.substring(s.indexOf(" ") + 1);
-	n = Integer.parseInt(s.substring(0,s.indexOf(" ")));
+	n = Integer.parseInt(s);
 	lakeMap = new int[r][c];
-	int currentRow = 0;
 	s = "";
-	while (currentRow < r){
+	for (int row = 0; row < r; row++){
 	    if (sc.hasNextLine()){
 		s = sc.nextLine();
-		int i = 0;
-		while (s.length() > -1) {
-		    lakeMap[currentRow][i] = Integer.parseInt(s.substring(0, indexOf(" ")));
-		    i ++;
+		for (int col = 0; col < c; col++){
+		    lakeMap[row][col] = Integer.parseInt(s.substring(0, s.indexOf(" ")));
+		    s = s.substring(s.indexOf(" ") + 1);
 		}
 	    }
 	    else throw new IllegalArgumentException("Lake grid not formatted correctly");
 	}
-}
+	instructions = new String[n];
+	s = "";
+	for (int i = 0; i < n; i++){
+	    if (sc.hasNextLine()){
+		instructions[i] = sc.nextLine();
+	    }
+	    else throw new IllegalArgumentException("instructions not formatted correctly");
+	}
+	if (sc.hasNext()){
+	    throw new IllegalArgumentException("first line does not correspond to rest of file");
+	}
+        for (int i = 0; i < instructions.length; i ++) {
+	    s = instructions[i];
+	    int row = Integer.parseInt(s.substring(0, s.subtring(0, s.indexOf(" "))));
+	    s = s.substring(s.indexOf(" ") + 1);
+	    int col = Integer.parseInt(s.substring(0, s.subtring(0, s.indexOf(" "))));
+	    s = s.substring(s.indexOf(" ") + 1);
+	    int decrease = Integer.parseInt(s);
+	    int largest = lakeMap[row][col];
+	    for (int Ro = row; Ro < row + 3; Ro ++) {
+		for (int Co = col; Co < col + 3; Co ++) {
+		    if (lakeMap[Ro][Co] > largest) {
+			largest = lakeMap[Ro][Co];
+		    }
+		}
+	    }
+	    int maxHeight = largest - decrease;
+	    for (int Ro = row; Ro < row + 3; Ro ++) {
+		for (int Co = col; Co < col + 3; Co ++) {
+		    if (lakeMap[Ro][Co] > maxHeight) {
+			lakeMap[Ro][Co] = maxHeight;
+		    }
+		}
+	    }
+    }
+
+    //for i in {1..10}; do hava USCO makelake.$i

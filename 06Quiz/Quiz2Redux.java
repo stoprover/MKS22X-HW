c5bc83738c220f899c1605cdc4208dfa1fc2b0a2
stoprover <steventoprover@gmail.com>
2017-03-21 14:31:45 -0400

    quiz works?

--- 06Quiz/Quiz2Redux.java
+++ 06Quiz/Quiz2Redux.java
@@ -2,6 +2,9 @@ import java.util.*;
 public class Quiz2Redux{  
     public static ArrayList<String> combinations(String s){
 	ArrayList<String>words = new ArrayList<String>();
+	if(s.length() == 0){
+	    return words;
+	}
 	help( words , s);
 	Collections.sort(words);
 	return words;

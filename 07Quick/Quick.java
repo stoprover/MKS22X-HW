e60ef0c2b8356bef2bb8ac1b8654c7df58452024
Galina Gorina <Galina Gorina>
2017-03-21 23:58:38 -0400

    partition fully working also i am too tired to do anything else so good night

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -4,8 +4,13 @@ public class Quick{
 	Random r = new Random();
 	int pivot = r.nextInt((end - start) + 1);
 	pivot += start;
-	//int pivot = 1;
-	int atPivot = data[pivot]; //3
+	if (data.length == 0){
+	    int[]ary = new int[2];
+	    ary[0] = start;
+	    ary[1] = end;
+	    return ary;
+	}
+	int atPivot = data[pivot];
 	System.out.println("pivot: " + pivot);
 	System.out.println("atPivot: " + atPivot);
 	int i = start;
@@ -29,100 +34,21 @@ public class Quick{
 		gt --;
 	    }
 	}
-
-
-
-
-		
-
-
-	/**for (int i = start; i < pivot; i++){
-	    if (data[i] > atPivot){
-		int temp = data[i];
-		for (int j = i; j < end; j++){
-		    data[j] = data[j + 1];
-		}
-		data[end] = temp;
-		i--;
-		System.out.println("1st \n" + toString(data));
-	    }
-	}
-	for (int i = start; i <= end; i++){
-	    if (data[i] == atPivot){
-		pivot = i;
-		break;
-	    }
-	}
-	/**for (int k = pivot + 1; k <= end; k++){
-	    if (data[k] < atPivot){
-		int temp = data[k];
-		for (int j = k; j > start; j--){
-		    data[j] = data[j - 1];
-		}
-		data[start] = temp;
-		k--;
-		System.out.println("2nd \n" + toString(data));
-	    }
-	    }*/
-
-	/**while (q >= start){
-	   
-	    if (data[p] >= atPivot){
-		int temp = data[p];
-		for (int j = p; j < end; j++){
-		    data[j] = data[j + 1];
-		}
-		data[end] = temp;
-	    }
-	    if (data[q] < atPivot){
-		p++;
-		//int temp = data[start];
-	    }
-	    q--;
-	}
-
-	/**while (i <= end){
-	    if (data[i] > atPivot){
-		int temp = data[i];
-		for (int j = i; j < end; j++){
-		    data[j] = data[j + 1];
-		}
-		data[end] = temp;
-		i++;
-	    }
-	    else {
-		i = i;
-	    }
-	    }*/
-	/**for (int i = start; i <= indOfChosen; i++){
-	    System.out.println("i: " + i);
-	    //int k = i;
-	    System.out.println("data[k] " + data[i]);
-	    if (data[i] > atPivot){
-		int temp = data[i];
-		for (int j = i; j < end; j++){
-		    data[j] = data[j + 1];
-		}
-		data[end] = temp;
-		i -= 1;
-		System.out.println(toString(data));
-		}*/
-	/**for (int k = start; k < end; k++){
-	    if (data[k] == atPivot){
-		indOfChosen = pivot;
-	    }
-	    }*/
-	
-	/**for (int l = start; l <= end; l++){
-	    if (data[l] == atPivot){
-		return l;
-	    }
-	    }*/
 	int[]ary = new int[2];
 	ary[0] = lt;
 	ary[1] = gt;
 	return ary;
     }
+    public static void quicksort(int[]ary){
+	//something needs to happen if it's length 0
+	part(ary, 0, part(ary, 0, ary.length - 1)[0]);
+	part(ary, part(ary, 0, ary.length - 1)[1], ary.length - 1);
+	//this is probably a high key disaster but idk what's going on anymore so good night
+	//for (int i = 0; i < ary.length - 1; i++){
+	//  if (ary[i] > ary[i + 1]){
+    }
+		
+	
     public static String toString(int[]ary){
 	String sum = "";
 	for (int i = 0; i < ary.length; i++){
@@ -132,7 +58,7 @@ public class Quick{
     }
     public static void main(String[]args){
 	int[]potato = new int[11];
-	potato[0] = 1; //v
+	/**potato[0] = 1; //v
 	potato[1] = 7; //14; //v
 	potato[2] = 23; //v
 	potato[3] = 4; //v
@@ -142,8 +68,8 @@ public class Quick{
 	potato[7] = 22; //v
 	potato[8] = 7; //144; //v
 	potato[9] = 2;
-	potato[10] = 12;
-	/**potato[0] = 999;
+	potato[10] = 12;*/
+	potato[0] = 999;
 	potato[1] = 999;
 	potato[2] = 999;
 	potato[3] = 4;
@@ -153,10 +79,12 @@ public class Quick{
 	potato[7] = 2;
 	potato[8] = 999;
 	potato[9] = 999;
-	potato[10] = 999;*/
+	potato[10] = 999;
+	int[]pop = new int[1];
+	
 	System.out.println("original: \n" + toString(potato));
 	int[]ary = new int[2];
-	System.out.println(part(potato, 1, 9));
+	System.out.println(part(potato, 3, 7));
 	//System.out.println(ary[0]);
 	//System.out.println(ary[1]);
 	//System.out.println(part(potato, 0, 10)[0]);

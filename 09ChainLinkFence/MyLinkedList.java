48e0047b5e8875eec561943f5582c814316107c1
stoprover <steventoprover@gmail.com>
2017-03-30 14:46:29 -0400

    exceptions, but otherwise doing ok

--- 09ChainLinkFence/MyLinkedList.java
+++ 09ChainLinkFence/MyLinkedList.java
@@ -16,10 +16,10 @@ public class MyLinkedList{
     }
 
     public MyLinkedList(){
-	head = new LNode (5);
-	size = 1;
+	head = new LNode (0);//what on earth is this
+	size = 0;
 	head.next = null;
-	System.out.println(head.value);
+	//System.out.println(head.value);
     }
     public boolean add(int val){
 	LNode newOne = new LNode(val, head);
@@ -27,7 +27,25 @@ public class MyLinkedList{
 	head = newOne;
 	return true;
     }
+    public int size(){
+	return size;
+    }
+    public int get(int index){
+	LNode current = head;
+	for (int i = 0; i < this.size(); i++){
+	    if (i == index){
+		return current.value;
+	    }
+	    else{
+		current = current.next;
+	    }
+	}
+	return -1;//errors: too high, too low, doesn't exist, others?
+    }
     public String toString(){
+	if (this.size() == 0){
+	    return "[]";
+	}
 	String sum = "[";
 	LNode current = head;
 	for (int i = 0; i < this.size; i++){
@@ -38,10 +56,17 @@ public class MyLinkedList{
 	sum += "]";
 	return sum;
     }
+
     public static void main(String[]args){
 	MyLinkedList idk = new MyLinkedList();
 	System.out.println(idk.toString());
 	System.out.println(idk.add(7));
+	System.out.println(idk.add(6));
 	System.out.println(idk.toString());
+	System.out.println(idk.get(4));
+	System.out.println("size: " + idk.size());
+	MyLinkedList emp = new MyLinkedList();
+	System.out.println("emp size: " + emp.size());
+	System.out.println("emp toString: " + emp.toString());
     }
 }

77f7c3cd216b48cf27d454f22d74b031fa89de89
Galina Gorina <Galina Gorina>
2017-04-19 22:39:56 -0400

    ok its semi-working, but get causes problems

--- 10WoodenDeck/MyDeque.java
+++ 10WoodenDeck/MyDeque.java
@@ -1,4 +1,31 @@
-public interface MyDeque<E>{
-    LinkedList leDeque;
-    int frontInd;
-    int backInd;
+import java.util.*;
+public class MyDeque{
+    private LinkedList<String> leDeque;
+    private int frontInd;
+    private int backInd;
+    public MyDeque(){
+	leDeque = new LinkedList();
+	leDeque.add("fdfd");
+	int frontInd = 0;
+	int backInd = 0;
+    }
+    public void wat(){
+	String l = leDeque.get(frontInd);
+	System.out.println(l);
+    }
+    
+    public String getFirst(){
+	return leDeque.get(frontInd);
+    }
+    public String getLast(){
+	return leDeque.get(backInd);
+	}
+    public static void main(String[]args){
+	MyDeque f = new MyDeque();
+	f.wat();
+	
+	System.out.println(f.getFirst());
+	System.out.println(f.getLast());
+	
+    }
+}

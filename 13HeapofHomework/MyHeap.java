4e4188fb43acf6dd4ed0cb60821510ec44f2aab7
stoprover <steventoprover@gmail.com>
2017-05-03 14:47:38 -0400

    ahhhh

---
+++ 13HeapofHomework/MyHeap.java
@@ -0,0 +1,23 @@
+public class MyHeap{
+    private int cons;
+    private ArrayList<String> heap;
+    public MyHeap(){
+	cons = 1;
+	heap = new ArrayList<String>();
+    }
+    public MyHeap(boolean whichKind){
+	if (whichKind){
+	    cons = 1;
+	}
+	else{
+	    cons = -1;
+	}
+	heap = new ArrayList<String>();
+    }
+    public String rChild(){
+	return get(2 * get(this.uiindexOf????_) + 1?????);
+	public void lChild()
+	public void parent()
+	public void add (String s)
+	public String remove()
+	public String peek()

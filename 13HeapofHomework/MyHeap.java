69e9180a91a68c83f7782488174124db602541ba
stoprover <steventoprover@gmail.com>
2017-05-08 14:47:38 -0400

    omg please i just want it to work i want to go to bed

--- 13HeapofHomework/MyHeap.java
+++ 13HeapofHomework/MyHeap.java
@@ -1,10 +1,11 @@
 import java.util.*;
 public class MyHeap{
     private int cons;
-    private ArrayList<String> heap;
+    private ArrayList<String> pile;
     public MyHeap(){
 	cons = 1;
-	heap = new ArrayList<String>();
+	pile = new ArrayList<String>();
+	pile.add(null);
     }
     public MyHeap(boolean whichKind){
 	if (whichKind){
@@ -13,44 +14,81 @@ public class MyHeap{
 	else{
 	    cons = -1;
 	}
-	heap = new ArrayList<String>();
-	heap.add(null);
+	pile = new ArrayList<String>();
+	pile.add(null);
     }
     
-    public String rChild(){
-	return heap.get(2 * heap.indexOf(this) + 1);
+    public String rChild(String examined){
+	return pile.get(2 * pile.indexOf(examined) + 1);
     }
-    public String lChild(){
-	return heap.get(2 * heap.indexOf(this));
+    public String lChild(String examined){
+	return pile.get(2 * pile.indexOf(examined));
     }
-    public String parent(){
-	return heap.get(heap.indexOf(this) / 2);
+    public String parent(String examined){
+	return pile.get(pile.indexOf(examined) / 2);
     }
     /**public void add (String s)
 	public String remove()
 	public String peek()*/
-    private void pushUp(){//there are this problems
-	if (thi.compareTo(this.parent()) * cons > 0){
-	    int thisInd = heap.indexOf(this);
-	    int parInd = heap.indexOf(this.parent());
-	    heap.set(thisInd, this.parent());
-	    heap.set(parInd, this);
-	    this.pushUp();
+    private void pushUp(int pos){//there are this problems
+	String examined = pile.get(pos);
+	if (examined.compareTo(parent(examined)) * cons < 0){
+	    int thisInd = pos;
+	    int parInd = pile.indexOf(parent(examined));
+	    pile.set(thisInd, parent(examined));
+	    pile.set(parInd, examined);
+	    pushUp(pos / 2);
 	}
 	else{
 	    return;
 	}
     }
-    public String printHeap(){
+    public void add(String s){
+	pile.add(s);
+	pushUp(pile.size() - 1);
+    }
+    public String toString(){
 	String sum = "";
-	for (int i = 0; i < heap.size(); i++){
-	    sum += heap.get(i);
+	for (int i = 1; i < pile.size(); i++){
+	    sum += pile.get(i) + " " + parent(pile.get(i)) + ",\n";
 	}
 	return sum;
     }
     public static void main(String[]args){
 	MyHeap h = new MyHeap();
-	System.out.println(h.printHeap());
-    }}
-		
+	h.add("n");
+	h.add("j");
+	h.add("p");
+	h.add("x");
+	h.add("e");
+	h.add("r");
+	h.add("g");
+	h.add("p");
+	h.add("c");
+	h.add("i");
+	h.add("i");
+	h.add("r");
+	h.add("r");
+	h.add("l");
+	h.add("l");
+	h.add("d");
+	h.add("s");
+	h.add("h");
+	h.add("u");
+	h.add("f");
+	h.add("i");
+	h.add("d");
+	h.add("p");
+	h.add("v");
+	h.add("w");
+	h.add("s");
+	/**h.add("string");
+	h.add("potato");
+	h.add("apple");
+	h.add("orange");*/
+	//xSystem.out.println(h.parent("d"));
 
+
+	System.out.println(h.toString());
+    }
+}

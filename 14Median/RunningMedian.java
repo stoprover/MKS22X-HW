1eb83e29adc90e5e063b493411867bb0255a7b34
Galina Gorina <Galina Gorina>
2017-06-12 19:13:42 -0400

    Finally finished this

--- 14Median/RunningMedian.java
+++ 14Median/RunningMedian.java
@@ -12,68 +12,39 @@ public class RunningMedian{
 	else if (rightHeap.size() == 0){
 	    rightHeap.add(i);
 	}
+	else if (leftHeap.size() == rightHeap.size()){
+	    if (i > leftHeap.peek()){
+		leftHeap.add(rightHeap.peek());
+		rightHeap.remove();
+		rightHeap.add(i);
+	    }
+	    else{
+		leftHeap.add(i);
+	    }
+	}
 	else if (leftHeap.size() > rightHeap.size()){
-	    rightHeap.add(i);
-	}
-	else if (leftHeap.size() < rightHeap.size()){
-	    leftHeap.add(i);
-	}
-	else{
-	    leftHeap.add(i);
+	    if (i > leftHeap.peek()){
+		rightHeap.add(i);
+	    }
+	    else{
+		rightHeap.add(leftHeap.peek());
+		leftHeap.remove();
+		leftHeap.add(i);
+	    }
 	}
     }
     public double getMedian(){
 	if (leftHeap.size() == rightHeap.size()){
-	    return (leftHeap.peek() + rightHeap.peek()) / 2;
+	    if (leftHeap.size() == 0){
+		throw new IndexOutOfBoundsException();
+	    }
+	    return (leftHeap.peek() + rightHeap.peek()) / 2.0;
 	}
 	else if (leftHeap.size() > rightHeap.size()){
 	    return leftHeap.peek();
 	}
 	return rightHeap.peek();
     }
-    public static void main(String[]args){
-	RunningMedian r = new RunningMedian();
-	r.add(1);
-	System.out.println("left: " + r.leftHeap.printList());
-	r.add(1);
-	System.out.println("left: " + r.leftHeap.printList());
-	System.out.println("right: " + r.rightHeap.printList());
-	r.add(8);
-	System.out.println("left: " + r.leftHeap.printList());
-	System.out.println("right: " + r.rightHeap.printList());
-	r.add(4);
-	System.out.println("left: " + r.leftHeap.printList());
-	System.out.println("right: " + r.rightHeap.printList());
-	r.add(3);
-	System.out.println("left: " + r.leftHeap.printList());
-	System.out.println("right: " + r.rightHeap.printList());
-	r.add(1);
-	System.out.println("left: " + r.leftHeap.printList());
-	System.out.println("right: " + r.rightHeap.printList());
-	r.add(1);
-	System.out.println("left: " + r.leftHeap.printList());
-	System.out.println("right: " + r.rightHeap.printList());
-	r.add(3);
-	System.out.println("left: " + r.leftHeap.printList());
-	System.out.println("right: " + r.rightHeap.printList());
-	r.add(2);
-	System.out.println("left: " + r.leftHeap.printList());
-	System.out.println("right: " + r.rightHeap.printList());
-	r.add(3);
-	System.out.println("left: " + r.leftHeap.printList());
-	System.out.println("right: " + r.rightHeap.printList());
-	r.add(7);
-	System.out.println("left: " + r.leftHeap.printList());
-	System.out.println("right: " + r.rightHeap.printList());
-	r.add(4);
-	System.out.println("left: " + r.leftHeap.printList());
-	System.out.println("right: " + r.rightHeap.printList());
-	r.add(2);
-	System.out.println("left: " + r.leftHeap.printList());
-	System.out.println("right: " + r.rightHeap.printList());
-	r.add(8);
-	System.out.println("left: " + r.leftHeap.printList());
-	System.out.println("right: " + r.rightHeap.printList());
-	System.out.println(r.getMedian());
+    public static void main(String[]args){	
     }
 }
